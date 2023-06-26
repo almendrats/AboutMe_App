5692abf1fc6ed371adda8e147b4ea9ec72036f0e
almendrats <almendrats23@gmail.com>
2023-06-26 09:31:01 -0500

    Initial Commit

---
+++ AboutMe_App/AboutMe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMe_AppApp.swift
+//  AboutMe_App
+//
+//  Created by Almendra Tantajulca on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
